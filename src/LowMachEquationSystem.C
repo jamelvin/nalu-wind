31c46c7ad66d35df72f0efa2aeea5307239c5d7e
Jeremy Melvin <jamelvin@gmail.com>
2019-02-21 18:32:31 -0600

    Minor bug fixes.  Tested on channel case.  Runs for a while and then blows up after ~6000 steps in what appears to be the epsilon equation.  Further testing needed.
    Conflicts:
    	include/Enums.h
    	src/TurbKineticEnergyEquationSystem.C

--- src/LowMachEquationSystem.C
+++ src/LowMachEquationSystem.C
@@ -1480,7 +1480,7 @@ MomentumEquationSystem::register_interior_algorithm(
         case SST: case SST_DES: 
           theAlg = new TurbViscSSTAlgorithm(realm_, part);
           break;
-        case KE:
+        case KEPS:
           theAlg = new TurbViscChienKEAlgorithm(realm_, part);
           break;
         case TAMS:
